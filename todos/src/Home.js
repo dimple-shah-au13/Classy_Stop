bacd4fa7fd117d31782fb5616e28515d59bd35e4
dimple-shah-au13 <dimpleshah_28@yahoo.com>
2021-07-17 12:22:38 +0530

    created todolist

---
+++ todos/src/Home.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+import Masonry from 'react-masonry-css';
+import TakeNote from './TakeNote';
+
+
+
+const PinnedNote = ({state, removeFromNotes, removePin}) => {
+    if(state.pinned_id){
+      return (
+      <div className="pinned">
+        <h4>Pinned</h4>
+        {state.notes_list.filter(n=>{ return n.id === state.pinned_id }).map((item,index)=>
+        <p key={index} className="list-item" id="li">
+          <span className="span1">{item.title} <button className="pin-button" onClick={e=>removePin(item.id)}><img className="pin" src="./push-pin.png"/></button></span> 
+          <span className="span2">{item.input}</span>
+          <button className="list-button" onClick={e=>removeFromNotes(index,item.id)}>delete</button>
+        </p>
+        )}
+        <hr></hr>
+      </div>
+      );
+    }
+    else {
+        return null;
+    }
+}
+const Home = ({state, styles, ...actions}) => {
+  
+    // const list_string = state.notes_list
+    // const notes_list = JSON.parse(list_string);
+    const slice = state.notes_list.slice(state.offset, state.offset + state.perPage)
+
+    
+    console.log("slice",slice)
+    return(
+      <>
+         <div>
+        <TakeNote state={state} {...actions} />
+        <div className="popup" style={state.showPopUp ? styles.inputStyle : styles.inputStyle1}>
+            <p className="text" >
+            <label>
+          Set status:
+          <select value={state.edited_note.status}  onChange={(e)=>actions.handleChangeNote(e.target.value,"status", "edited_note")} className="edit-input" >
+          
+            <option value="Backlog">Backlog</option>
+            <option value="InProgress">InProgress</option>
+            <option value="Done">Done</option>
+          </select>
+        </label>
+              <span className="edit-title"><input value={state.edited_note.title} onChange={(e)=>actions.handleChangeNote(e.target.value,"title", "edited_note")}/> <button onClick={e=>actions.pinNote(state.popUp_id)}><img className="pin" src="./push-pin.png"/></button></span>
+              <input value={state.edited_note.input} onChange={(e)=>actions.handleChangeNote(e.target.value,"input", "edited_note")} className="edit-input" />
+              {/* <input value={state.edited_note.status} onChange={(e)=>actions.handleChangeNote(e.target.value,"status", "edited_note")} className="edit-input" /> */}
+              <button onClick={e=>actions.updateNote(state.popUp_id)} className="close">close</button>
+              <button onClick={e=>actions.removeFromNotes(state.popUp_id)} className="delete">delete</button>
+            </p>
+        </div>
+        <PinnedNote state={state} {...actions}/>
+        <ul>
+          <Masonry
+            breakpointCols={4}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column">
+            {(!state.search ? slice : state.search_list).filter(n=>{ return ((n.id !== state.pinned_id) && (n.id !== state.popUp_id)) }).map((item,index)=>
+              <li key={index} style={item.status==="InProgress" || item.status==="Backlog"?item.status==="InProgress"?{backgroundColor:"yellow"}:{backgroundColor:"red"}:{backgroundColor:"green"}}className="list-item" >
+                <span className="span1">{item.tag}<button className="pin-button" onClick={e=>actions.pinNote(item.id)}><img className="pin" src="./push-pin.png"/></button></span>
+                <span className="span1">Title: {item.title} </span> 
+                <span className="span2">Task: {item.input}</span> 
+                
+                <span className="span2">{item.status}</span>
+                <span className="span2">Due Date: { item.dueDate }</span>
+                <div className="action">
+                  <button className="list-button" onClick={e => actions.showNote(item.id)}>Edit</button>
+                <button className="list-button" onClick={e=>actions.removeFromNotes(index, item.id)}>Delete</button>
+                </div></li>)}
+          
+          </Masonry>
+        </ul>
+        </div>
+        <div className = "pagination">
+        <ReactPaginate
+            previousLabel={"prev"}
+            nextLabel={"next"}
+            breakLabel={"..."}
+            breakClassName={"break-me"}
+            pageCount={state.pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={actions.handlePageClick}
+            containerClassName={"pagination"}
+            subContainerClassName={"pages pagination"}
+            activeClassName={"active"}/>
+    </div>
+    </>
+    );
+}
+
+export default Home;
